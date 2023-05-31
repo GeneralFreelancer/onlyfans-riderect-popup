b0649b627a85a3d9f3dfacaf055335de3b900b72
FreelancerDen <freelancer.den.rud@gmail.com>
2023-05-31 15:18:20 +0300

    temp folder and structure

---
+++ script.js
@@ -0,0 +1,24 @@
+function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    const link = document.createElement('link');
+    link.href = url;
+    link.onload = resolve;
+    link.onerror = reject;
+    document.head.appendChild(link);
+  });
+}
+
+// Usage
+const cdnUrl = 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css';
+loadScript(cdnUrl)
+  .then(() => {
+    // Script loaded successfully
+    // You can now use the functionality provided by the loaded script
+  })
+  .catch((error) => {
+    // An error occurred while loading the script
+    console.error('Failed to load script:', error);
+  });
+
+  
+
